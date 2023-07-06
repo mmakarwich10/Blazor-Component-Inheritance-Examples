0e4be870eb879694462db864d0cb48ccefc36fc1
mmakarwich10 <mist_shaker@yahoo.com>
2023-07-05 21:03:56 -0500

    Added content.

---
+++ BlazorComponentInheritanceExamples/Components/SimpleDerivedComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorComponentInheritanceExamples.Components
+{
+    public class SimpleDerivedComponent : SimpleBaseComponent
+    { // Even though this is a normal class, because it's inheriting from a component, it will be treated as a component.
+        // Any class inheriting (however distantly) from ComponentBase will exhibit the same behavior.
+        protected override void OnInitialized()
+        {
+            Header = "Header from derived component";
+            base.OnInitialized();
+        }
+    }
+}
